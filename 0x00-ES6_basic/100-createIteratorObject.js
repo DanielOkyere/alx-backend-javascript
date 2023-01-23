85651895329022aa6bff64ed6b6245e4e165da36
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-01-23 19:23:42 +0000

    100-createIteratorObject

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject(report) {
+  const employee = [];
+  for (const [dept, emplys] of Object.entries(report.allEmployees)) {
+    for (const emp of emplys) {
+      employee.push(emp);
+    }
+  }
+
+  return employee;
+}
