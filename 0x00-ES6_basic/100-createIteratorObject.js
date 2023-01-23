ea693eec317b4b2846d9584b5942b807d423aff9
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-01-23 19:27:02 +0000

    fixed no unused-vars

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,5 @@
 export default function createIteratorObject(report) {
+  /* eslint-disable no-unused-vars */
   const employee = [];
   for (const [dept, emplys] of Object.entries(report.allEmployees)) {
     for (const emp of emplys) {
