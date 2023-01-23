27fc0dee538b6c0b3e69cafb57672fc9711f3d19
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-01-23 13:41:40 +0000

    Task 1 - Block Scope - done

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+/**
+ * Block scoped
+ */
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = false;
+    task2 = true;
+  }
+
+  return [task, task2];
+}
