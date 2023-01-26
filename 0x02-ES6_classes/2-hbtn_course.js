e85fb760555ffadd0a3d815cefb7cd84219a4444
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-01-26 20:59:55 +0000

    2-hbtn_course fix

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -13,4 +13,24 @@ export default class HolbertonCourse {
     if (typeof name !== 'string') throw TypeError('Name must be a string');
     this._name = name;
   }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if ((!Array.isArray(students) || students.every((student) => typeof student !== 'string'))) {
+      throw TypeError('Student must be an array of strings');
+    }
+    this._students = students;
+  }
 }
