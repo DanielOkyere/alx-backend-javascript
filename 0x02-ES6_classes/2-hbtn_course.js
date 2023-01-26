49e1e21de1d652b403322645654018eec5601759
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-01-26 20:46:34 +0000

    added holberton course

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,16 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    this._name = name;
+  }
+}
