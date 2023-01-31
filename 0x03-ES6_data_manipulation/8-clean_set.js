c02d06071734fa87a714a4474c328eef748bf8b7
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-01-31 10:04:40 +0000

    100-weak.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
-export default function cleanSet (set, string) {
-  const strings = []
-  if (string === '' || typeof string !== 'string') return ''
+export default function cleanSet(set, string) {
+  const strings = [];
+  if (string === '' || typeof string !== 'string') return '';
   set.forEach((s) => {
     if (typeof s === 'string' && s.startsWith(string)) {
-      strings.push(s.slice(string.length))
+      strings.push(s.slice(string.length));
     }
-  })
-  return strings.join('-')
+  });
+  return strings.join('-');
 }
