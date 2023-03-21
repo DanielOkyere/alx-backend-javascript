0bdcb803917ff83217934173f1f8850667be54f4
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2023-03-21 13:44:49 +0000

    Done with stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+/* Using Process stdin */
+console.log('Welcome to Holberton School, what is your name?');
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    console.log(`Your name is ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    console.log(`Your name is ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing');
+  });
+}
