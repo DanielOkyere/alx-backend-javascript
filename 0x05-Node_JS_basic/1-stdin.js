8dad74e98d53eecf84b1bc5f17dd10bbfbd6d5c4
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2023-03-21 13:50:16 +0000

    Changed console.log to process write

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,5 @@
 /* Using Process stdin */
-console.log('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
     console.log(`Your name is ${data.toString()}`);
