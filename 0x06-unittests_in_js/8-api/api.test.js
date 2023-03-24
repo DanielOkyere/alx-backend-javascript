532a6928579b1cc7129453e3efaadcfa22600f4d
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2023-03-24 17:09:17 +0000

    done api

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,8 +5,8 @@ describe('API integration test', () => {
   const API_URL = 'http://localhost:7865';
   it('GET / returns correct response', (done) => {
     request.get(`${API_URL}/`, (_err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to payment system');
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
