6c1234c7baf1e65d46c75fd58c12ffa9bed41dd3
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2023-03-24 14:01:55 +0000

    Added 5 payment

---
+++ 0x06-unittests_in_js/5-payment.js
@@ -0,0 +1,11 @@
+/**
+ * send Payment API
+ */
+
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  console.log(`The total is: ${Utils.calculateNumber('SUM', totalAmount, totalShipping)}`);
+}
+
+module.exports = sendPaymentRequestToApi;
