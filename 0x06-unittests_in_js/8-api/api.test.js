966c90685d0e3bba6540ad82190457557fa7a879
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2023-03-24 16:39:03 +0000

    8-api done

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to payment system');
+      done();
+    });
+  });
+});
